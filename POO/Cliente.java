0f967a50fdfcd97a6c0722febfb19bd1ce950dbf
Lucas Moreira <71292751+lucasmp159@users.noreply.github.com>
2022-11-25 16:05:37 -0300

    De Lucas Moreira

---
+++ POO/Cliente.java
@@ -0,0 +1,59 @@
+package Desafio2.PacoteModelo;
+
+public class Cliente {
+    private String cpf;
+    private String nome;
+    private String endereco;
+    private int fone;
+    private String email;
+
+    public String getCpf() {
+
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+
+        return nome;
+    }
+
+    public void setNome(String nome) {
+
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+
+        this.endereco = endereco;
+    }
+
+    public int getFone() {
+
+        return fone;
+    }
+
+    public void setFone(int fone) {
+
+        this.fone = fone;
+    }
+
+    public String getEmail() {
+
+        return email;
+    }
+
+    public void setEmail(String email) {
+
+        this.email = email;
+    }
+}
