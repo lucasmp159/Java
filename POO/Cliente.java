764b7bba4eff63865c7c2d16bef70b8e41a7ef37
Lucas Moreira <71292751+lucasmp159@users.noreply.github.com>
2022-12-06 12:15:48 -0300

    Add files via upload

--- POO/Cliente.java
+++ POO/Cliente.java
@@ -1,59 +1,19 @@
-package Desafio2.PacoteModelo;
+package Arraylist;
+
+import java.util.*;
 
 public class Cliente {
-    private String cpf;
-    private String nome;
-    private String endereco;
-    private int fone;
-    private String email;
-
-    public String getCpf() {
-
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-
-        this.cpf = cpf;
-    }
-
-    public String getNome() {
-
-        return nome;
-    }
-
-    public void setNome(String nome) {
-
-        this.nome = nome;
-    }
-
-    public String getEndereco() {
-
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-
-        this.endereco = endereco;
-    }
-
-    public int getFone() {
-
-        return fone;
-    }
-
-    public void setFone(int fone) {
-
-        this.fone = fone;
-    }
-
-    public String getEmail() {
-
-        return email;
-    }
-
-    public void setEmail(String email) {
-
-        this.email = email;
-    }
+	
+	public void cliente() {
+		
+		ArrayList<String> clientes = new ArrayList<String>();
+		clientes.add("Paulo");
+		clientes.add("Tereza");
+		clientes.add("João");
+		clientes.add("Maria");
+		if (clientes.contains("Maria")) {
+		clientes.remove(1);	
+		}
+		System.out.println(clientes);
+	}
 }
--- POO/Principal.java
+++ POO/Principal.java
@@ -1,33 +1,11 @@
-package Desafio2.PacoteControle;
-
-import Desafio2.PacoteModelo.Cliente;
-import Desafio2.PacoteModelo.Veiculo;
+package Arraylist;
 
 public class Principal {
-    public static void main(String[] args){
-    Cliente user = new Cliente();
-    Veiculo carro = new Veiculo();
 
-    user.setNome("Chico Nabudonosor Anísio");
-    System.out.println("Nome: " + user.getNome());
-    user.setCpf("328.123.647-10");
-    System.out.println("CPF: " + user.getCpf());
-    user.setEmail("Chicotudo@gmail.com");
-    System.out.println("Email: " + user.getEmail());
-    user.setEndereco("Rua 1 do lado da Rua 2");
-    System.out.println("Endereço: " + user.getEndereco());
-    user.setFone(998371238);
-    System.out.println("Telefone: " + user.getFone());
-    System.out.println("-------------------------------");
-    carro.setFabricante("Toyota");
-    System.out.println("Fabricante do Veiculo: " + carro.getFabricante());
-    carro.setModelo("Corolla 2.0 Altis Premium CVT");
-    System.out.println("Modelo: " + carro.getModelo());
-    carro.setPlaca("POC-4769");
-    System.out.println("Placa: " + carro.getPlaca());
-    carro.setCor("Branco");
-    System.out.print("Cor: " + carro.getCor());
-    carro.setAno(2023);
-    System.out.println("   || Ano: " + carro.getAno());
-    }
+	public static void main(String[] args) {
+		Impares obj = new Impares();
+		obj.impar();
+		Cliente obj1 = new Cliente(); 
+		obj1.cliente();
+	}
 }
